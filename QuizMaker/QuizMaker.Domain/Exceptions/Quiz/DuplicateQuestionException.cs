496fec422abd0a57d3d4c6af0c3082b2365f8be7
Ascic, Maid <maid.ascic@student.tugraz.at>
2026-02-10 20:23:35 +0100

    Removed DuplicateQuestionException

--- QuizMaker/QuizMaker.Domain/Exceptions/Quiz/DuplicateQuestionException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace QuizMaker.Domain.Exceptions.Quiz;
-internal class DuplicateQuestionException: DomainException {
-
-    internal DuplicateQuestionException(Guid quizId, Guid questionId) :
-        base($"Quiz {quizId} already contains question {questionId}") { }
-}
