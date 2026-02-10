bb94b1ec0e72f607cd503e57d288c44e5a3843e6
Ascic, Maid <maid.ascic@student.tugraz.at>
2026-02-10 20:26:42 +0100

    Added Quiz Unit Tests

---
+++ QuizMaker/QuizMaker.UnitTests/Domain/MemberDataForTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMaker.UnitTests.Domain;
+public static class MemberDataForTests {
+
+    public static IEnumerable<object?[]> InvalidStrings =>
+        new[]
+        {
+            new object?[] { null },
+            new object?[] { "" },
+        };
+
+    public static IEnumerable<object?[]> InvalidTitleAndMaxQuestions =>
+    new[]
+    {
+        new object?[] { "Test", null },
+        new object?[] { "Test", "" },
+        new object?[] { null, "Test" },
+        new object?[] { "", "Test" },
+    };
+}
